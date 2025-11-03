3966cb0569ae6deafaa825d16de8aa452a54200c
Madzia <erbert.magdalena@gmail.com>
2025-11-03 16:40:20 +0100

    Added tests

--- src/test/java/HRCalculatorTest.java
+++ src/test/java/HRCalculatorTest.java
@@ -2,11 +2,25 @@ import org.junit.jupiter.api.Test;
 import se2526.HRCalculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class HRCalculatorTest {
+
     @Test
     public void testValidMaxHR() {
         var result = HRCalculator.calculateMaxHR(25);
         assertEquals(195, result);
+    }
 
+    @Test
+    public void testInvalidMinHR_ageTooHigh() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(500));
+    }
+
+    @Test
+    public void testInvalidMinHR_ageTooLow() {
+        assertThrows(IllegalArgumentException.class,
+                () -> HRCalculator.calculateMaxHR(-1));
     }
 }
